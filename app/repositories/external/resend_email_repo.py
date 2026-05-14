a082b77281149ca81d095cc3ca31a01e39b39015
Karla <karla.schra@gmail.com>
2026-05-14 15:13:43 +0200

    Changed mail domain, and sender

--- app/repositories/external/resend_email_repo.py
+++ app/repositories/external/resend_email_repo.py
@@ -8,7 +8,7 @@ resend_api_key = os.environ.get("RESEND_API_KEY")
 class ResendEmailRepo:
     def send_email(self, subject: str, body: str, recipient: str):
         r = resend.Emails.send({
-            "from": "makeascene@bebelebeb.de",
+            "from": "no-reply@makeascene.social",
             "to": recipient,
             "subject": subject,
             "html": body
